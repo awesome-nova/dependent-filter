8a49df04e1a4eaefee33155c29999b92e587ec04
Dmytro Kulyk <dmytro@dkulyk.name>
2020-03-06 16:05:33 +0200

    Added route names

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{resource}/filters/options', 'FilterController@options');
-Route::get('/{resource}/lens/{lens}/filters/options', 'LensFilterController@options');
+Route::get('/{resource}/filters/options', 'FilterController@options')->name('dependent-filter.resource.options');
+Route::get('/{resource}/lens/{lens}/filters/options', 'LensFilterController@options')->name('dependent-filter.lens.options');
